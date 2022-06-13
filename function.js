6a34e7e16311a0b62ab803915bf5e76e5c877df9
Nettester <github@gluch.eu>
2022-06-13 14:48:52 +0200

    init structure

---
+++ function.js
@@ -0,0 +1,19 @@
+let text=document.getElementById("text");
+let list=document.getElementById("list");
+
+function add() {
+    return true;
+}
+
+function del() {
+    return true;
+}
+
+function edit() {
+    return true;
+}
+
+function checked() {
+    return true;
+}
+
