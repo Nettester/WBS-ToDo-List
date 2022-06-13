73fdee8ccf33faaa52f0ef4230466a3050efc7c8
Nettester <github@gluch.eu>
2022-06-13 15:09:59 +0200

    com

--- function.js
+++ function.js
@@ -1,18 +1,34 @@
 let text=document.getElementById("text");
 let list=document.getElementById("list");
 
+/*
+    Achim
+
+    tr get id
+*/
 function add() {
     return true;
 }
 
+/*
+    Seo
+
+    del tr with id
+*/
 function del() {
     return true;
 }
 
+/*
+   David 
+*/
 function edit() {
     return true;
 }
 
+/*
+  Abdulaziz  
+*/
 function checked() {
     return true;
 }
